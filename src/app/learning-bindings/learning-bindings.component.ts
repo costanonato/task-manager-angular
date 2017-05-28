8a8c4ba7e97cc8f491b260b18f4fdaa173e606d8
Costa Nonato <costa@nonato.com>
2017-05-27 22:18:55 -0300

    binding from logic to view

--- src/app/learning-bindings/learning-bindings.component.ts
+++ src/app/learning-bindings/learning-bindings.component.ts
@@ -6,22 +6,43 @@ import { Component } from "@angular/core";
 })
 
 export class LearningBindingsComponent {
+
+  public mouseClickCount: number;
+  public mouseOverCount: number;
+  public userName: string;
+  public userEmail: string;
+
+  public constructor(){
+    this.mouseClickCount = 0;
+    this.mouseOverCount = 0;
+    this.userName = "Nome Inicial";
+    this.userEmail = "Email Inicial";
+  }
+
+
+
   // mouse events
   public onClick(){
     console.log('Evento onClick disparado!');
+    this.mouseClickCount++;
   }
 
   public onMouseOver(){
     console.log('Evento onMouseOver disparado!');
+    this.mouseOverCount++;
   }
 
 
   // key events
-  public onKeyDown(){
+  public onKeyDown(event: any){
     console.log('Evento onKeyDown disparado!');
+    console.log(event);
+    this.userName = event.target.value;
   }
 
-  public onKeyUp(){
+  public onKeyUp(event: any){
     console.log('Evento onKeyUp disparado!');
+    console.log(event);
+    this.userEmail = event.target.value;
   }
 }
