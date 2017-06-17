de4f79ce1138055d1e6dd4c6d8627d2e31b01b49
Costa Nonato <costa@nonato.com>
2017-06-17 09:46:51 -0300

    Adding #getTask to TaskService

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -35,4 +35,10 @@ export class TaskService{
     return Promise.resolve(TASKS.slice(0, 3));
   }
 
+
+  public getTask(id: number): Promise<Task> {
+    return this.getTasks()
+      .then(tasks => tasks.find(task => task.id === id))
+  }
+
 }
