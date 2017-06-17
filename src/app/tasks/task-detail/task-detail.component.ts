c3e5f251afd0baccd1c3d7b18ed3e1f5c69b47bf
Costa Nonato <costa@nonato.com>
2017-06-17 10:34:25 -0300

    getting params from TaskDetailComponent route

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,14 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
+
+import "rxjs/add/operator/switchMap";
 
 import { Task } from '../shared/task.model';
+import { TaskService } from '../shared/task.service';
 
 @Component({
   selector: 'task-detail',
   templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent{
+export class TaskDetailComponent implements OnInit{
   @Input() public task: Task;
 
-  public constructor(){ }
+  public constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute
+  ){ }
+
+  public ngOnInit(){
+    this.route.params
+      .switchMap((params: Params) => this.taskService.getTask(+params['id']))
+      .subscribe(task => this.task = task)
+  }
 }
