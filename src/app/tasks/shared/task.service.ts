0a97507c43c24553348b742d3f87107732978ea9
Costa Nonato <costa@nonato.com>
2017-06-28 15:28:45 -0300

    adding http to TaskService

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -1,3 +1,4 @@
+import { Http } from "@angular/http";
 import { Injectable } from "@angular/core";
 
 import { Task } from "./task.model";
@@ -16,6 +17,7 @@ const TASKS: Array<Task> = [
 @Injectable()
 
 export class TaskService{
+  public constructor(private http: Http){}
 
   public getTasks(): Promise<Task[]>{
     let promise = new Promise((resolve, reject) => {
