419f29f10b63bfbc473b02a701c5f02d2a1659f9
Costa Nonato <costa@nonato.com>
2017-07-12 10:36:17 -0300

    adding tasks

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -40,6 +40,17 @@ export class TaskService{
   }
 
 
+  public createTask(task: Task): Observable<Task> {
+    let url = this.tasksUrl;
+    let body = JSON.stringify(task);
+    let headers = new Headers({'Content-Type': 'application/json'});
+
+    return this.http.post(url, body, { headers: headers })
+      .catch(this.handleErrors)
+      .map(response => response.json().data as Task)
+  }
+
+
   public updateTask(task: Task): Observable<Task> {
     let url = `${this.tasksUrl}/${task.id}`;
     let body = JSON.stringify(task);
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,9 +10,11 @@ import { TaskService } from './shared/task.service';
 
 export class TasksComponent implements OnInit{
   public tasks: Array<Task>;
-  public selectedTask: Task;
+  public newTask: Task;
 
-  public constructor(private taskService: TaskService){  }
+  public constructor(private taskService: TaskService){ 
+    this.newTask = new Task(null, '');
+  }
 
   public ngOnInit(){
     this.taskService.getTasks()
@@ -21,8 +23,22 @@ export class TasksComponent implements OnInit{
         error => alert("Ocorreu um no servidor, tente mais tarde.")
       )
   }
+  
+  public createTask(){
+    this.newTask.title = this.newTask.title.trim();
 
-  public onSelect(task: Task): void {
-    this.selectedTask = task;
+    if(!this.newTask.title){
+      alert("A tarefa deve ter um título");
+    }else{
+      this.taskService.createTask(this.newTask)
+        .subscribe(
+          (task) => {
+            this.tasks.push(task);
+            this.newTask = new Task(null, '');
+          },
+          () => alert("Ocorreu um no servidor, tente mais tarde.")
+        )
+    }
   }
+
 }
