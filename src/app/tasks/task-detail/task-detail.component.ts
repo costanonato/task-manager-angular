c966729301f173b643aed255bf12c05d059ee363
Costa Nonato <costa@nonato.com>
2017-08-09 19:06:03 -0300

    adding validations

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
 import { Location } from "@angular/common";
-import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 
 import { Task } from '../shared/task.model';
 import { TaskService } from '../shared/task.service';
@@ -26,9 +26,9 @@ export class TaskDetailComponent implements OnInit, AfterViewInit{
     private formBuilder: FormBuilder
   ){ 
     this.reactiveTaskForm = this.formBuilder.group({
-      title: [null],
-      deadline: [null],
-      done: [null],
+      title: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(255)]],
+      deadline: [null, Validators.required],
+      done: [null, Validators.required],
       description: [null]
     })
   }
