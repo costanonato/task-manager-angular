e8c2d4362396f1eaa02134ef5f761dbac4235abf
Costa Nonato <costa@nonato.com>
2017-09-13 09:58:55 -0300

    adding SignUpFormComponent base class

---
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'sign-up-form',
+  templateUrl: './sign-up-form.component.html'
+})
+
+export class SignUpFormComponent {
+  public userForm: FormGroup;
+
+  public constructor(private formBuilder: FormBuilder){
+    this.userForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(7), Validators.maxLength(100)]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      password_confirmation: [null, [Validators.required]]
+    })
+  }
+
+  public signUpUser(){
+    console.log("Formulário de SignUp enviado!");
+    console.log(this.userForm.value)
+  }
+}
