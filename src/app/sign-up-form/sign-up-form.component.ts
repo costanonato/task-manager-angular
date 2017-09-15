447a30c49119daf640495bf1ec78db43e832c08f
Costa Nonato <costa@nonato.com>
2017-09-15 11:23:02 -0300

    adding validations and messages to SignUpFormComponent

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,25 +1,30 @@
 import { Component } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
+import { FormUtils } from "../shared/form.utils";
+
 @Component({
   selector: 'sign-up-form',
   templateUrl: './sign-up-form.component.html'
 })
 
 export class SignUpFormComponent {
-  public userForm: FormGroup;
+  public form: FormGroup;
+  public formUtils: FormUtils;
 
   public constructor(private formBuilder: FormBuilder){
-    this.userForm = this.formBuilder.group({
-      name: [null, [Validators.required, Validators.minLength(7), Validators.maxLength(100)]],
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required, Validators.minLength(8)]],
       passwordConfirmation: [null, [Validators.required]]
     })
+
+    this.formUtils = new FormUtils(this.form);
   }
 
   public signUpUser(){
     console.log("Formulário de SignUp enviado!");
-    console.log(this.userForm.value)
+    console.log(this.form.value);
   }
 }
