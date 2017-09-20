69b0f484c11fdd8b708b3b014e3efbd8cf681c0a
Costa Nonato <costa@nonato.com>
2017-09-20 10:59:01 -0300

    adding SignUpFormComponent to AppModule and AppRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { SignInFormComponent } from "./sign-in-form/sign-in-form.component"
 import { SignUpFormComponent } from "./sign-up-form/sign-up-form.component"
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
 
 const ROUTES = RouterModule.forRoot([
+  { path: 'sign-in', component: SignInFormComponent },
   { path: 'sign-up', component: SignUpFormComponent },
   { path: 'tasks/:id', component: TaskDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { SignInFormComponent } from "./sign-in-form/sign-in-form.component";
 import { SignUpFormComponent } from "./sign-up-form/sign-up-form.component";
 import { TaskSearchComponent } from "./navbar/task-search/task-search.component";
 import { TasksComponent } from "./tasks/tasks.component";
@@ -44,6 +45,7 @@ import * as datetimepicker from 'eonasdan-bootstrap-datetimepicker';
     AppComponent,
     DashboardComponent,
     NavbarComponent,
+    SignInFormComponent,
     SignUpFormComponent,
     TaskSearchComponent,
     TasksComponent,
