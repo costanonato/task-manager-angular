5f0816d3f5f1f95318701c48199a47acbebcd349
Costa Nonato <costa@nonato.com>
2017-09-29 11:15:26 -0300

    handling backEnd SignUp errors

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -15,18 +15,33 @@ import { User } from "../shared/user.model";
 export class SignUpFormComponent {
   public form: FormGroup;
   public formUtils: FormUtils;
+  public submitted: boolean;
+  public formErrors: Array<string>;
 
   public constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router){
     this.setupForm();
     this.formUtils = new FormUtils(this.form);
+    this.submitted = false;
+    this.formErrors = null;
   }
 
   public signUpUser(){
+    this.submitted = true;
+
     this.authService.signUp(this.form.value as User)
       .subscribe(
         () => {
           alert('Parabéns, sua conta foi criada com sucesso!');
           this.router.navigate(['/dashboard']);
+          this.formErrors = null;
+        },
+        (error) => {
+          this.submitted = false;
+
+          if( error.status === 422 )
+            this.formErrors = JSON.parse(error._body).errors.full_messages;
+          else
+            this.formErrors = ["Não foi possível processar a sua solicitação. Por favor, tente mais tarde."]
         }
       )
   }
